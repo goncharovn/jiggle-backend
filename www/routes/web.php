9ef8b1f7c1c90debbd4071d120b8324f112f4cb7
Nikita <nagoncharov11@gmail.com>
2023-08-16 12:49:38 +0700

    task-37 - www/routes/web.php - fix: change regulars

--- www/routes/web.php
+++ www/routes/web.php
@@ -10,83 +10,83 @@ use app\Controllers\ResetPasswordController;
 use app\Controllers\BasketController;
 
 return [
-    '/^$/' => [
+    '#^$#' => [
         'controller' => MainPageController::class,
         'action' => 'index'
     ],
-    '/^p/(?<id>\d+)$/' => [
+    '#^p/(?<id>\d+)$#' => [
         'controller' => ProductController::class,
         'action' => 'index',
     ],
-    '/^remove-product$/' => [
+    '#^remove-product$#' => [
         'controller' => ProductController::class,
         'action' => 'removeProductFromBasket',
     ],
-    '/^my-account$/' => [
+    '#^my-account$#' => [
         'controller' => AccountPagesController::class,
         'action' => 'index'
     ],
-    '/^my-account/order-history$/' => [
+    '#^my-account/order-history$#' => [
         'controller' => AccountPagesController::class,
         'action' => 'orderHistory'
     ],
-    '/^my-account/delivery-address$/' => [
+    '#^my-account/delivery-address$#' => [
         'controller' => AccountPagesController::class,
         'action' => 'deliveryAddress'
     ],
-    '/^my-account/my-details$/' => [
+    '#^my-account/my-details$#' => [
         'controller' => AccountPagesController::class,
         'action' => 'myDetails'
     ],
-    '/^signup$/' => [
+    '#^signup$#' => [
         'controller' => SignupController::class,
         'action' => 'signup'
     ],
-    '/^confirm-signup$/' => [
+    '#^confirm-signup$#' => [
         'controller' => SignupController::class,
         'action' => 'confirmSignup'
     ],
-    '/^login$/' => [
+    '#^login$#' => [
         'controller' => LoginController::class,
         'action' => 'login'
     ],
-    '/^login/disable-2fa$/' => [
+    '#^login/disable-2fa$#' => [
         'controller' => LoginController::class,
         'action' => 'disableMultiFactorAuth'
     ],
-    '/^login/enable-2fa$/' => [
+    '#^login/enable-2fa$#' => [
         'controller' => LoginController::class,
         'action' => 'enableMultiFactorAuth'
     ],
-    '/^login/process-2fa$/' => [
+    '#^login/process-2fa$#' => [
         'controller' => LoginController::class,
         'action' => 'processMultiFactorAuth'
     ],
-    '/^reset-password$/' => [
+    '#^reset-password$#' => [
         'controller' => ResetPasswordController::class,
         'action' => 'resetPassword'
     ],
-    '/^change-password$/' => [
+    '#^change-password$#' => [
         'controller' => ResetPasswordController::class,
         'action' => 'changePassword'
     ],
-    '/^confirm-email$/' => [
+    '#^confirm-email$#' => [
         'controller' => ProfileController::class,
         'action' => 'confirmEmail'
     ],
-    '/^change-name$/' => [
+    '#^change-name$#' => [
         'controller' => ProfileController::class,
         'action' => 'changeName'
     ],
-    '/^change-email$/' => [
+    '#^change-email$#' => [
         'controller' => ProfileController::class,
         'action' => 'changeEmail'
     ],
-    '/^signout$/' => [
+    '#^signout$#' => [
         'controller' => ProfileController::class,
         'action' => 'signout'
     ],
-    '/^basket$/' => [
+    '#^basket$#' => [
         'controller' => BasketController::class,
         'action' => 'index'
     ],
