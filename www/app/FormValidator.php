cd06e4c9c8a9be65bba341d9315d2ada7b4b4619
Nikita <nagoncharov11@gmail.com>
2023-08-22 22:11:07 +0700

    task-37 - FormValidator.php - refactor: replace elseif with foreach

--- www/app/FormValidator.php
+++ www/app/FormValidator.php
@@ -8,8 +8,6 @@ class FormValidator
     {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             ErrorMessagesManager::addNewMessage('formError', 'Invalid email.');
-        } elseif (strlen($email) > 255) {
-            ErrorMessagesManager::addNewMessage('formError', 'Email address is too long.');
         } else {
             return true;
         }
@@ -19,19 +17,19 @@ class FormValidator
 
     public static function isValidPassword($password): bool
     {
-        if (strlen($password) < '6') {
-            ErrorMessagesManager::addNewMessage('formError', 'Password must contain at least 6 characters.');
-        } elseif (!preg_match("#[0-9]+#", $password)) {
-            ErrorMessagesManager::addNewMessage('formError', 'Password must contain at least 1 number.');
-        } elseif (!preg_match("#[A-Z]+#", $password)) {
-            ErrorMessagesManager::addNewMessage('formError', 'Password must contain at least 1 capital letter.');
-        } elseif (!preg_match("#[a-z]+#", $password)) {
-            ErrorMessagesManager::addNewMessage('formError', 'Password must contain at least 1 lowercase letter.');
-        } elseif (strlen($password) > '128') {
-            ErrorMessagesManager::addNewMessage('formError', 'Password must not contain more than 128 characters.');
-        }
-        else {
-            return true;
+        $errorMessages = [
+            strlen($password) < 6 => 'Password must contain at least 6 characters.',
+            !preg_match("#[0-9]+#", $password) => 'Password must contain at least 1 number.',
+            !preg_match("#[A-Z]+#", $password) => 'Password must contain at least 1 capital letter.',
+            !preg_match("#[a-z]+#", $password) => 'Password must contain at least 1 lowercase letter.',
+            strlen($password) > 128 => 'Password must not contain more than 128 characters.'
+        ];
+
+        foreach ($errorMessages as $condition => $message) {
+            if ($condition) {
+                ErrorMessagesManager::addNewMessage('formError', $message);
+                return false;
+            }
         }
 
         return false;
